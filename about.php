22d9c09131470684259fe7f25ee375bbd45dab81
Gitanjali Deepak Bhaiswar <91129383+GitanjaliBhaiswar@users.noreply.github.com>
2022-11-07 22:16:47 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>SPARKS BANK</title>
+    <link rel="stylesheet" href="css/style3.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body >
+    <!-- Nav bar -->
+    <nav class="navbar py-2 navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">SPARKS BANK <img src="logos/logo.webp" alt="Bank" width="50" height="50"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-8 mb-lg-0">
+                    <li class="nav-item py-3">
+                        <a class="nav-link" aria-current="page" href="index.php"><strong>Home</strong></a>
+                    </li>
+                    <li class="nav-item dropdown py-3">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Actions
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item py-3" href="money.php">Money Transfer</a></li>
+                            <li><a class="dropdown-item py-3" href="#">Transaction History</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item py-3">
+                        <a class="nav-link" href="index.php">About</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+<center>
+        <div class="container" style="margin-top: 5%; padding:10px 80px 10px 80px; ">
+            <div
+                style="width:100%; background-color:gray; padding:5px 5px 5px 5px; border-radius:30px; box-shadow: 2px 2px 10px gray;">
+                <h1 style=" color:black;">About Us</h1>
+            </div>
+          
+
+            <br><br>
+            <img src="bg7.jpg" alt="" style="width:90%,; box-shadow:2px 2px 15px black;">
+            <br><br>
+            <p style="color:white;">This website is created by</p>
+            <h1 style="color:black; text-shadow:2px 2px 2px gray;">Gitanjali Bhaiswar</h1>
+            <p style="color:gray;"><b>Intern at Sparks Foundation <br> Sipna College Of Engineering And Technology,Amravati.</b></p>
+    </center>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
